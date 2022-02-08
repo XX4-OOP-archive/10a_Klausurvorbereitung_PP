680380ea19992b79b3a646e2da5ab1ecb3372937
Peter <schmidt.multimedia@gmail.com>
2022-02-08 08:38:16 +0100

    Primitives / Arrays

--- funktionen.js
+++ funktionen.js
@@ -1,2 +1,39 @@
 /* DATENSTRUKTUREN */
 
+/* Primitives */
+
+// let a // (var) Definition oder Deklaration
+
+// // Wertzuweisung / Assignment
+// a = true; // boolean (true, false)
+// a = "Hallo"; // string
+// a = 2; // number
+
+// console.log(typeof a);
+// console.log(a);
+
+/* Arrays */
+
+// let arr;
+// arr = new Array();
+// arr = [true,false,false,true];
+
+// Index beginnt mit 0
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[3]);
+// console.log(arr.length);
+// console.log(arr[arr.length-1]);
+// console.log(arr[5]);
+
+/* Array -Info, nicht klausurrelevant*/
+// let arr = [];
+// console.log(arr);
+// arr.push("a");
+// console.log(arr);
+// arr.push("b");
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
